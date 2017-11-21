b7d415c41735559807382165fc7585a312e56155
mattpng <mpng@wisc.edu>
2017-11-21 17:26:06 -0600

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,13 +1,32 @@
+/////////////////////////////////////////////////////////////////////////////
+// Semester:         CS367 Fall 2017 
+// PROJECT:          P4 Research Geneology
+// FILE:             Queue.java
+//
+// TEAM:    P4 Pair 32
+// Authors: Matt P'ng, Jasper Nelson
+// Author1: Matt P'ng, mpng@wisc.edu, mpng, 002
+// Author2: Jasper Nelson, jnelson27@wisc.edu, jnelson27, 002
+//
+// ---------------- OTHER ASSISTANCE CREDITS 
+// Persons: NA
+// 
+// Online sources: NA
+//////////////////////////// 80 columns wide //////////////////////////////////
+
 
 public class Queue<T> implements QueueADT<T> {
 	
 	public LinkedList<T> q;
 	
+//	Constructor 
 	public Queue(){
 		q = new LinkedList<T>();
 	}
 	
-
+	/**
+	 * @return true if the Queue is empty
+	 */
 	public boolean isEmpty() {
 		if (q.size()==0){
 			return true;
@@ -15,7 +34,12 @@ public class Queue<T> implements QueueADT<T> {
 		return false;
 	}
 
-
+	/**
+	 * Adds an item to the back of a queue
+	 * 
+	 * @return true if the Queue is empty
+	 * @throws IllegalArgumentException if data is null
+	 */
 	public void enqueue(T data) throws IllegalArgumentException {
 		if (data == null){
 			throw new IllegalArgumentException();
@@ -23,23 +47,27 @@ public class Queue<T> implements QueueADT<T> {
 		q.add(data);
 	}
 
-	
+	/**
+	 * Return and remove the next item (from the front) of the queue,
+	 *
+     * @throws EmptyQueueException if queue is empty
+     */
 	public T dequeue() throws EmptyQueueException {
 		if (isEmpty()) {
 			throw new EmptyQueueException();
 		}
-	
 		return q.remove(0);
 	}
 
-	
+	/**
+	 * Get and do not remove the next item in the queue,
+     *
+     * @throws EmptyQueueException if queue is empty 
+     */
 	public T element() throws EmptyQueueException {
 		if (isEmpty()) {
 			throw new EmptyQueueException();
 		}
-
 		return q.get(0);
 	}
-
-	
 }
